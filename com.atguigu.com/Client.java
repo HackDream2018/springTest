a242766162240590b037fed1ae7bf83a3499abf8
HackDream2018 <18569069613@163.com>
2019-10-14 09:18:30 +0800

    10.14

---
+++ com.atguigu.com/Client.java
@@ -0,0 +1,15 @@
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @version v1.0
+ * @author: TianXiang
+ * @description:
+ * @date: 2019/10/4
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    }
+    
+}
