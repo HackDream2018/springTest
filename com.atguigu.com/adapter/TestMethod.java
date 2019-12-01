4884a7b27f1e6eb6afd916a4de24eff3242490a1
HackDream2018 <18569069613@163.com>
2019-12-01 17:04:08 +0800

    12.1

--- com.atguigu.com/adapter/TestMethod.java
+++ com.atguigu.com/adapter/TestMethod.java
@@ -1,5 +1,8 @@
 package adapter;
 
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
 /**
  * @version v1.0
  * @author: TianXiang
@@ -9,7 +12,9 @@ package adapter;
 public class TestMethod {
     
     public static void main(String[] args) {
-
+        SpelExpressionParser expParser = new SpelExpressionParser();
+        Expression expression = expParser.parseExpression("100*(2+400)*1+66");
+        System.out.println(expression.getValue());
     }
     
 }
